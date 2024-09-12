cb75d5b801eaa18a090e1b9d503e88cd337bfbc6
srikanta <srikanta.ku.subudhi@gmail.com>
2024-09-12 08:43:17 +0530

    5.2 completed

---
+++ core/presentation/ui/src/main/java/com/sri/core/presentation/ui/DataErrorToText.kt
@@ -0,0 +1,33 @@
+package com.sri.core.presentation.ui
+
+import com.sri.core.domain.util.DataError
+
+fun DataError.asUiText(): UiText{
+    return when(this) {
+        DataError.Local.DISK_FULL -> UiText.StringResource(
+            R.string.error_disk_full
+        )
+        DataError.Network.REQUEST_TIMEOUT -> UiText.StringResource(
+            R.string.error_request_timeout
+        )
+        DataError.Network.UNAUTHORIZED -> UiText.StringResource(
+            R.string.error_unauthorized
+        )
+        DataError.Network.NO_INTERNET -> UiText.StringResource(
+            R.string.error_no_internet
+        )
+        DataError.Network.PAYLOAD_TOO_LARGE -> UiText.StringResource(
+                R.string.error_payload_too_large
+            )
+        DataError.Network.SERVER_ERROR -> UiText.StringResource(
+            R.string.error_server_error
+        )
+        DataError.Network.SERIALIZATION -> UiText.StringResource(
+            R.string.error_serialization
+        )
+        else -> UiText.StringResource(
+            R.string.error_unknown
+        )
+    }
+
+}
