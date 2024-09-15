bf1633e2e0e94d045da2a54aa68481bcf9bce657
srikanta <srikanta.ku.subudhi@gmail.com>
2024-09-15 10:01:44 +0530

    6.3 completed

---
+++ auth/data/src/main/java/com/sri/auth/data/EmailPatternValidator.kt
@@ -0,0 +1,10 @@
+package com.sri.auth.data
+
+import android.util.Patterns
+import com.sri.auth.domain.PatternValidator
+
+object EmailPatternValidator: PatternValidator {
+    override fun matches(value: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(value).matches()
+    }
+}
--- auth/presentation/src/main/java/com/sri/auth/presentation/register/RegisterViewModel.kt
+++ auth/presentation/src/main/java/com/sri/auth/presentation/register/RegisterViewModel.kt
@@ -1,16 +1,54 @@
-@file:Suppress("OPT_IN_USAGE_FUTURE_ERROR")
+@file:OptIn(ExperimentalComposeUiApi::class)
+
 package com.sri.auth.presentation.register
 
+
+import androidx.activity.result.launch
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.runtime.snapshots.SnapshotStateObserver
 
-class RegisterViewModel: ViewModel() {
+import androidx.compose.ui.semantics.text
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sri.auth.domain.UserDataValidator
+import androidx.compose.ui.text.input.TextFieldValue
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+
+class RegisterViewModel(private val userDataValidator: UserDataValidator): ViewModel() {
     var state by mutableStateOf(RegisterState())
         private set
 
+    init{
+        viewModelScope.launch {
+            snapshotFlow { state.email.text }
+                .onEach { email ->
+                    state = state.copy(
+                        isEmailValid = userDataValidator.isValidEmail(email.toString())
+                    )
+                }
+            snapshotFlow { state.password.text }
+                .onEach { password ->
+                    state = state.copy(
+                        passwordValidationState = userDataValidator.validatePassword(password.toString())
+                    )
+                }
+
+        }
+
+    }
     fun onAction(action: RegisterAction){
 
     }
 }
+
+
+
+
